8b63378f5ffc5d28c9298da25444c0215376cc48
Kania47 <skilllu11@gmail.com>
2024-09-05 12:53:16 +0200

    skopiowane

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    //Nazwa pliku i nazwa klasy muszą być takie same
+    //nazwy klas powinny być z wielkiej litery - tak karze obyczaj
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Witamy na lekcjach programowania");
+        //typ prosty - tylko wartośc, z małej litery
+        /*
+        boolean,
+        int long  short byte
+        float double
+        char
+         */
+        //typ zlozony - obiekt ktory ma walansosci i metody, z wielkiej litery
+        int wylosowanaLiczba = (int)(Math.random()*10+1);//rzutowanie
+
+        System.out.println(wylosowanaLiczba);
+
+        System.out.println("Zgadnij liczbę");
+        Scanner klawiatura = new Scanner(System.in);
+        int wpisanaLiczba = klawiatura.nextInt();
+
+        if(wpisanaLiczba == wylosowanaLiczba){
+            System.out.println("Gratulacje wygrana");
+        }
+        else {
+            System.out.println("Pudło");
+        }
+
+    }
+}
